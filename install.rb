b26bf9a4e4b063a7b0ebde4051173eba7ae85dbc
Jose Fernandez <jose@umn.edu>
2008-05-13 18:42:29 -0500

    Added install script

--- install.rb
+++ install.rb
@@ -1,1 +1,5 @@
-# Install hook code here
+require 'fileutils'
+
+cj_config = File.dirname(__FILE__) + '/../../../config/cj_devkey.yml'
+FileUtils.cp File.dirname(__FILE__) + '/cj_devkey.yml.tpl', cj_config unless File.exist?(cj_config)
+puts IO.read(File.join(File.dirname(__FILE__), 'README'))
--- lib/cj4r.rb
+++ lib/cj4r.rb
@@ -1,1 +1,13 @@
-# Cj4r
+require 'rubygems'
+gem 'soap4r'
+
+module Cj4r
+  
+  def self.included(base)
+    include ClassMethods
+    mattr_reader :developer_key
+    
+    @@developer_key = 
+  end
+  
+end
