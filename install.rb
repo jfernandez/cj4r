44615891cc43f868bf1bc62ac7be3d1f541bac04
Jose Fernandez <jose@umn.edu>
2008-06-20 10:25:24 -0500

    Correctly output the README when running the install script

--- install.rb
+++ install.rb
@@ -2,4 +2,4 @@ require 'fileutils'
 
 cj_config = File.dirname(__FILE__) + '/../../../config/cj_key.yml'
 FileUtils.cp File.dirname(__FILE__) + '/cj_key.yml.tpl', cj_config unless File.exist?(cj_config)
-puts IO.read(File.join(File.dirname(__FILE__), 'README'))
+puts IO.read(File.join(File.dirname(__FILE__), 'README.markdown'))
